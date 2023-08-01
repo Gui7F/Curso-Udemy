6005fe02d76a504f0c270cc314686b200055dfff
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2023-07-31 23:29:14 -0300

    Conta com prompt

---
+++ Java Script/Java Script Básico/exe02/script.js
@@ -0,0 +1,9 @@
+let numero1 = prompt('Digite um número')
+let numero2 = prompt ('Digite outro numero')
+
+numero1 = parseFloat (numero1)
+numero2 = parseFloat (numero2)
+
+let resultado = numero1 + numero2
+
+alert (`O resultado é ${resultado}`)
