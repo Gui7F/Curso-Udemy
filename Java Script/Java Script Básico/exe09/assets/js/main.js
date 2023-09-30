172009654c0656d2d6c65127b13f2278b5052782
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2023-09-30 02:46:58 -0300

    Update main.js

--- Java Script/Java Script Básico/exe09/assets/js/main.js
+++ Java Script/Java Script Básico/exe09/assets/js/main.js
@@ -1,74 +1,71 @@
-// Muda o padrão (Default) do formulario
+//Mudar o padrão do formulario 
 
-const formulario = document.querySelector('#form') ;
+const form = document.querySelector('#formulario');
+formulario.addEventListener('submit', function (evento) {
+   evento.preventDefault()
 
-formulario.addEventListener('submit', function (event){
+   const inputPeso = evento.target.querySelector('#Pesovalor')
+   const inputAltura = evento.target.querySelector('#Alturavalor')
 
-  event.defaultPrevented()
+   const peso = Number(inputPeso.value);
+   const altura = Number(inputAltura.value);
 
-  const inputPeso = event.target.querySelector('#Peso')
-  const inputAltura= event.target.querySelector('#Altura')
+   if (!peso) {
+      resultados('Peso invalido', false);
+      return;
+   }
 
-  const peso = Number.inputPeso.value ;
-  const altura = Number.inputAltura.value;
- 
-  if(!peso) {
-    setResultado ('Peso Inválido' , false);
-    return;
-  }
+   if (!altura) {
+      resultados('Altura invalida', false);
+      return;
+   }
 
-  if(!altura){
-    setResultado('Altura Inválida', false)
-    return ;
-  }
+   const imc = getIMC(peso, altura);
+   const nivelIMC= getNivelImc(imc)
 
-  const imc = getImc(peso,altura);
-  const nivelImc = getNivelImc(imc);
-  const msg = `Seu IMC é ${imc} (${nivelImc})`
+   console.log(imc, nivelIMC)
 
+
+   function getIMC(peso, altura) {
+      const imc = peso / altura ** 2;
+      return imc.toFixed(2)
+   }
+
+   const msg = `Seu IMC é ${imc} (${nivelIMC})`
+
+  resultados(msg, true)
 
 })
 
-function getNivelImc(imc){
-  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 
-  'Obesidade grau 1', 'Obesidade grau 2' , 'Obesidade grau 3']
+function getNivelImc(imc) {
+   const nivel =
+      ['Abaixo do peso', 'Peso Normal', 'Acima do Peso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
 
-  if(imc >= 39.9) return nivel[5]
-  if(imc >= 34.9) return nivel[4]
-  if(imc >= 29.9) return nivel[3]
-  if(imc >= 24.9) return nivel[2]
-  if(imc >= 18.5) return nivel[1]
-  if(imc < 18.5)  return nivel[0]
-}
-
-function getImc(peso , altura){
-  const imc = peso / altura **2;
-  return imc.toFixed(2)
-
+   if (imc >= 39.9) return nivel[5]
+   if (imc >= 34.9) return nivel[4]
+   if (imc >= 29.9) return nivel[3]
+   if (imc >= 24.9) return nivel[2]
+   if (imc >= 18.9) return nivel[1]
+   if (imc < 18.9) return  nivel[0]
+  
 }
 
 
 
-function criaP (){
-    const p = document.createElement("p")
-    return p
+function criaP() {
+   const p = document.createElement('p');
+   return p;
+
+  
 }
 
+function resultados(msg, isValid) {
+   const resultado = document.querySelector('#resultado')
+   resultado.innerHTML = '';
+   const p = criaP()
+   if(isValid){ p.classList.add('valido', true)}
+   else{p.classList.add('invalido', false)}
+   p.innerHTML = msg
+   resultado.appendChild(p)
 
-
-function setResultado (msg, isValid){
-    const resultado = document.querySelector("#resultado");
-    resultado.innerHTML="";
-    
-    const p = criaP();
-
-    if(isValid){
-      p.classList.add('valido');
-
-    } else {
-      p.classList.add('Invalido')
-    }
-
-    p.innerHTML =  msg;
-    resultado.appendChild(p)
 }
