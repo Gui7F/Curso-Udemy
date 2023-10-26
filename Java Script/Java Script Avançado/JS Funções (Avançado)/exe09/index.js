5c53b642149b09227b5f7f8034e677b506c16353
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2023-10-26 03:56:26 -0300

    Calculadora com Função Construtora

---
+++ Java Script/Java Script Avançado/JS Funções (Avançado)/exe09/index.js
@@ -0,0 +1,81 @@
+function Calculadora() {
+    this.display = document.querySelector('.display')
+
+
+    this.inicia = () => {
+        this.interaçãoClick()
+        this.enterTecla()
+    }
+
+    this.interaçãoClick = () => {
+        document.addEventListener('click', e => {
+            const element = e.target;
+
+            if (element.classList.contains('button'))
+                this.addvalorDisplay(element.innerText)
+
+            if (element.classList.contains('button-clear')) {
+                this.clearDisplay()
+            }
+
+            if (element.classList.contains('button-del')) {
+                this.deletarUM()
+            }
+
+            if (element.classList.contains('button-eq')) {
+                this.calcularValores()
+            }
+
+        })
+    }
+
+
+   this.enterTecla = () =>{
+        document.addEventListener('keyup', e =>{
+            if (e.keyCode === 13){
+                this.calcularValores()
+            }
+        })
+    }
+
+    this.calcularValores = () => {
+        let conta = this.display.value
+
+        try {
+            conta = eval(conta)
+
+            if (!conta) {
+                alert('Conta Inválida')
+                return
+            }
+
+            this.display.value = String(conta)
+        } catch (e) {
+            alert('Conta Inválida')
+            return
+        }
+    }
+
+
+    this.deletarUM = () => {
+        this.display.value = this.display.value.slice(0, -1)
+    }
+
+
+
+    this.clearDisplay = () => {
+        this.display.value = ''
+    }
+
+
+
+    this.addvalorDisplay = element => {
+        this.display.value += element
+    }
+
+
+
+}
+
+const calculadora = new Calculadora()
+calculadora.inicia()
