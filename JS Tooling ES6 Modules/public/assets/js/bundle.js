b908468788d0f7f60e25b93c68e1afac2b9dbd9c
Any <80535297+Gui7F@users.noreply.github.com>
2024-01-22 01:44:41 -0300

    Webpack (Boilerplate)

---
+++ JS Tooling ES6 Modules/public/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//
+//
+//
+console.log('olaaaa');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
